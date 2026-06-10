82f5f6c119ab03a70889bac8838baf29fcbd4506
Ryan Mac <ryan.mac.rm4@gmail.com>
2026-06-10 19:57:00 -0400

    Analyse de compléxité asymptotique only for sorting algorithms

--- Cargaisons.java
+++ Cargaisons.java
@@ -11,36 +11,36 @@ public class Cargaisons implements Comparable<Cargaisons> {
         this.distance = 0.0;
     }
 
-    // COMPLEXITÉ: O(1) => exécute un nbre fixe de constantes, indépendamment de la taille des données d'entrée
+    // Comparer les cargaisons en fonction de la distance, latitude, et longitude
     @Override
     public int compareTo(Cargaisons c) {
-        // Utilisation de comparaisons directes (<, >) pour éviter les approximations de virgule flottante
+        // Utilisation de comparaisons avec (<, >) pour éviter les approximations de virgule flottante
         
         // Plus proche distance.
         if (this.distance < c.distance) {
-            return -1; // O(1)
+            return -1;
         }
         if (this.distance > c.distance) {
-            return 1; // O(1)
+            return 1;
         }
 
         // Plus petite latitude.
         if (this.coords[0] < c.coords[0]) {
-            return -1; // O(1)
+            return -1;
         }
         if (this.coords[0] > c.coords[0]) {
-            return 1; // O(1)
+            return 1;
         }
 
         // Plus petite longitude.
         if (this.coords[1] < c.coords[1]) {
-            return -1; // O(1)
+            return -1;
         }
         if (this.coords[1] > c.coords[1]) {
-            return 1; // O(1)
+            return 1;
         }
 
-        return 0; // O(1)
+        return 0;
     }
     
     @Override
